ead7c5d79a832d5b3745e973faa0530714e662b8
Nico <nkaosartor@gmail.com>
2023-05-14 04:58:06 -0700

    added lanes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const ctx = canvas.getContext('2d');
 
 const width = (canvas.width = 200);
 
-const car = new Car(100, 100, 30, 50);
+const road = new Road(width / 2, width * 0.9, Math.abs(width / 50));
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 car.draw(ctx);
 
 animate();
@@ -12,6 +13,8 @@ function animate() {
 	car.update();
 	// setting the height here will clear the canvas on each frame so that the car doesn't leave a trail
 	const height = (canvas.height = window.innerHeight);
+
+	road.draw(ctx);
 	car.draw(ctx);
 
 	requestAnimationFrame(animate);
